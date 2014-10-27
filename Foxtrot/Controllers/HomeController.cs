a69356caf87874bc29e360a9e5b9f4f75e55aadb
Marcel <marcel@marcelmata.com>
2014-10-27 01:35:49 -0500

    Based on the membership type we no longer show the "Add Profile" button.

--- Foxtrot/Controllers/HomeController.cs
+++ Foxtrot/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Foxtrot.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,23 @@ namespace Foxtrot.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db;
+
+        public HomeController()
+        {
+            db = ApplicationDbContext.Create();
+        }
+
         public ActionResult Index()
         {
             if (Request.IsAuthenticated)
+            {
+                var user = db.Profiles.SingleOrDefault(u => u.Email == HttpContext.User.Identity.Name);
+                
+                ViewData["IsManager"] = user.Class == Core.EmployeeClass.Management;
+
                 return View();
+            }
 
             return RedirectToAction("Login", "Account");
         }
